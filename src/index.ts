f63e39a6360375607b619c04c941271052f926a8
navel3 <435569+navel3@users.noreply.github.com>
2020-09-16 02:10:01 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,80 @@
-import { commands, CompleteResult, ExtensionContext, listManager, sources, workspace } from 'coc.nvim';
+import {
+  commands,
+  CompleteResult,
+  ExtensionContext,
+  listManager,
+  sources,
+  workspace,
+  LanguageClient,
+  ServerOptions,
+  LanguageClientOptions,
+  services,
+  OutputChannel,
+} from 'coc.nvim';
 import DemoList from './lists';
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  workspace.showMessage(`coc-gauge works!`);
+  const serverOptions: ServerOptions = {
+    command: 'gauge',
+    args: ['daemon', '--lsp', '--dir', '.'],
+  };
+
+  // TODO: More precise selector
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: ['spec'],
+  };
+
+  const client = new LanguageClient('gauge', 'gauge', serverOptions, clientOptions);
+
+  const gaugeChannel = workspace.createOutputChannel('Gauge');
+
+  await workspace.nvim.command('au BufWinEnter output:///Gauge set syntax=markdown');
+
+  const getCurrentFileName = () => workspace.uri.replace(/^file:\/\/\//, '/');
+
+  let gaugeProc: ChildProcessWithoutNullStreams | undefined;
+  const runGauge = async ({ specFile, line }: { specFile: string; line?: number }) => {
+    gaugeChannel.clear();
+    gaugeChannel.show(true);
+
+    if (gaugeProc) {
+      workspace.showMessage('Gauge is running.', 'error');
+      return;
+    }
+
+    gaugeProc = spawn('gauge', ['run', '--simple-console', `${specFile}${line ? ':' + line.toString() : ''}`]);
+    gaugeProc.stdout.on('data', (chunk) => {
+      gaugeChannel.append(chunk.toString());
+    });
+    gaugeProc.stderr.on('data', (e) => {
+      gaugeChannel.append(e.message);
+    });
+    gaugeProc.on('close', (code) => {
+      gaugeChannel.appendLine(`Closed: ${code}`);
+    });
+    gaugeProc.on('exit', (code) => {
+      gaugeChannel.appendLine(`Exited: ${code}`);
+      gaugeProc = undefined;
+    });
+  };
 
   context.subscriptions.push(
-    commands.registerCommand('coc-gauge.Command', async () => {
-      workspace.showMessage(`coc-gauge Commands works!`);
+    services.registLanguageClient(client),
+
+    commands.registerCommand('coc-gauge.RunScenarioOnCursor', async () => {
+      const state = await workspace.getCurrentState();
+      runGauge({ specFile: getCurrentFileName(), line: state.position.line });
+    }),
+
+    commands.registerCommand('coc-gauge.RunSpec', async () => {
+      runGauge({ specFile: getCurrentFileName() });
+    }),
+
+    commands.registerCommand('coc-gauge.Stop', async () => {
+      if (gaugeProc) {
+        gaugeProc.kill();
+      }
     }),
 
     listManager.registerList(new DemoList(workspace.nvim)),
